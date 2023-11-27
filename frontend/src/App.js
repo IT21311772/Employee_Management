082c35ad8b88a5a0421b8ccd34be659ff948780c
IT21311772 <it21311772@my.sliit.lk>
2023-11-27 09:52:32 +0530

    finished login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from './Components/Login';
 import SignUp from './Components/SignUp';
+import Employees from './Components/DisplayData';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-          <Route path='/' element={<SignUp />} />
+          <Route path='/' element={<Login />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/employees' element={<Employees />} />
       </Routes>
     </div>
   );
--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
+import { useNavigate, Link } from 'react-router-dom';
+import axios from 'axios';
+import '../Styles/Signup.css';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError('');
+
+    const loginData = { email, password };
+
+    try {
+      const response = await axios.post('api/user/login', loginData);
+
+      if (response.status === 200) {
+        const responseData = response.data;
+        console.log('User logged in successfully', responseData);
+        navigate('employees');
+      } else {
+        const errorData = response.data;
+        console.error('User login failed', errorData);
+        setError('Invalid Credentials, Please try again');
+      }
+    } catch (error) {
+      console.error('Request Error', error);
+      setError('An Error occured during the login');
+    }
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className='container'>
+      <div className="login">
+        <h1>Administrator Login</h1>
+        <Form onSubmit={handleLogin} className='form'>
+          <Form.Group>
+            <Form.Control
+              name='email'
+              placeholder='Enter Your Email'
+              required
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              style={{width: "80%", height: "3vh"}}
+            />
+
+            <Form.Control
+              name='password'
+              placeholder='Enter Your Password'
+              required
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              style={{width: "80%", height: "3vh"}}
+            />
+            {error && <p className='error'>{error}</p>}
+          </Form.Group>
+          <button className='loginBtn'>Login</button>
+        </Form>
+        <p>Don't have an account ? <Link to = '/signup' className="link">SignUp</Link></p>
+      </div>
     </div>
   )
 }
 
-export default Login
+export default Login;
