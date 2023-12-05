5de48ff2735e2234fc891fc9658ae63f8013c85d
Rishen T.Lithan <100280958+IT21311772@users.noreply.github.com>
2023-12-05 10:13:40 +0530

    Done Update Operation

--- frontend/src/Components/DisplayData.js
+++ frontend/src/Components/DisplayData.js
@@ -1,9 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../Styles/Data.css';
+import { Form, Modal } from 'react-bootstrap';
 
 function DisplayData() {
   const [data, setData] = useState([]);
+  const [updatedData, setUpdatedData] = useState({});
+  const [show, setShow] = useState(false);
+  
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const updatePost = (data) => {
+    setUpdatedData(data);
+    handleShow();
+  }
+
+  const saveUpdatedData = () => {
+    axios.put(`/api/emp/update/${updatedData._id}`, updatedData)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+
+    handleClose();
+    window.location.reload();
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setUpdatedData((prev) => {
+      return {
+        ...prev,
+        [name] : value,
+      };
+    });
+  };
 
   useEffect(() => {
     axios.get("/api/emp/get")
@@ -17,6 +48,56 @@ function DisplayData() {
   return (
     <div className='content'>
       <h1>Employee Data</h1>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update Employee</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <Form.Control
+                name='id'
+                value={updatedData.id ? updatedData.id : "" }
+                onChange={handleChange}
+              />
+              <Form.Control
+                name='name'
+                value={updatedData.name ? updatedData.name : "" }
+                onChange={handleChange}
+              />
+              <Form.Control 
+                name='NIC'
+                value={updatedData.NIC ? updatedData.NIC : "" }
+                onChange={handleChange}
+              />
+              <Form.Control 
+                name='address'
+                value={updatedData.address ? updatedData.address : "" }
+                onChange={handleChange}
+              />
+              <Form.Control 
+                name='position'
+                value={updatedData.position ? updatedData.position : "" }
+                onChange={handleChange}
+              />
+              <Form.Control 
+                name='salary'
+                value={updatedData.salary ? updatedData.salary : "" }
+                onChange={handleChange}
+              />
+              <Form.Control 
+                name='contact'
+                value={updatedData.contact ? updatedData.contact : "" }
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <button onClick={saveUpdatedData}>Save Changes</button>
+          <button onClick={handleClose}>Close</button>
+        </Modal.Footer>
+      </Modal>
       {data ? (
         <table className='table'>
           <thead>
@@ -43,7 +124,7 @@ function DisplayData() {
                 <td>{post.position}</td>
                 <td>{post.salary}.00</td>
                 <td>{post.contact}</td>
-                <td><button>Update</button></td>
+                <td><button onClick={() => updatePost(post)}>Update</button></td>
                 <td><button>Delete</button></td>
               </tr>
             </tbody>
